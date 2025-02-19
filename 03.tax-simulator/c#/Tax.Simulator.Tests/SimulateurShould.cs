33d8e9bed54be1aad54490e02ee15ff5199d19bf
Nordine <nordine.hida@gmail.com>
2025-02-19 09:46:24 +0100

    test: calcul des impôts pour un couple marié/pacsé avec des enfants
    Une méthode de test existante `CalculerImpotsAnnuel_CoupleMariePacse` a été supprimée.
    Ajout d'une nouvelle méthode de test `CalculerImpotsAnnuel_CoupleMariePacseAvecEnfants`.
    Cette méthode vérifie le calcul de l'impôt annuel pour un couple marié ou pacsé avec des enfants.
    Elle teste deux cas :
    - Un cas valide avec trois enfants, résultat attendu : 3983,37.
    - Un cas invalide avec un nombre d'enfants négatif, levant une `ArgumentException` avec le message "Le nombre d'enfants ne peut pas être négatif."
    Les assertions vérifient l'exception et le type, ainsi que le résultat du calcul valide.

--- 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
+++ 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
@@ -32,7 +32,6 @@ public class SimulateurShould
         Assert.Equal(valeurAttendue, resultatValide);
     }
 
-
     [Fact(DisplayName = "Calcul de l'impôt pour un couple marié ou pacsé")]
     public void CalculerImpotsAnnuel_CoupleMariePacse()
     {
@@ -50,6 +49,29 @@ public class SimulateurShould
         Simulateur.CalculerImpotsAnnuel(situationFamiliale, salaireMensuelValide, salaireMensuelConjointInvalide, nombreEnfants));
 
         Assert.Equal("Les salaires doivent être positifs.", exception.Message);
+        Assert.IsType<ArgumentException>(exception);
+
+        Assert.Equal(valeurAttendue, resultatValide);
+
+    }
+
+    [Fact(DisplayName = "Calcul de l'impôt pour un couple marié ou pacsé avec des enfants")]
+    public void CalculerImpotsAnnuel_CoupleMariePacseAvecEnfants()
+    {
+        decimal valeurAttendue = 3983.37M;
+
+        string situationFamiliale = "Marié/Pacsé";
+        int salaireMensuel = 3000;
+        int salaireMensuelConjoint = 3000;
+        int nombreEnfantsValide = 3;
+        int nombreEnfantsInvalide = -1;
+
+        decimal resultatValide = Simulateur.CalculerImpotsAnnuel(situationFamiliale, salaireMensuel, salaireMensuelConjoint, nombreEnfantsValide);
+
+        var exception = Assert.Throws<ArgumentException>(() =>
+        Simulateur.CalculerImpotsAnnuel(situationFamiliale, salaireMensuel, salaireMensuelConjoint, nombreEnfantsInvalide));
+
+        Assert.Equal("Le nombre d'enfants ne peut pas être négatif.", exception.Message);
         Assert.IsType<ArgumentException>(exception);
 
         Assert.Equal(valeurAttendue, resultatValide);
