d14d860b5e73a1a8a36202e2d7a2c15e80dbc0b4
Nordine <nordine.hida@gmail.com>
2025-02-19 09:19:54 +0100

    test: vérification que le salaire d'un célibataire est strictement positif
    1. Salaire mensuel valide (2000) avec un résultat attendu de 1515.25.
    2. Salaire mensuel invalide (-2000) qui doit lever une exception `ArgumentException` avec le message "Les salaires doivent être positifs."
    3. Salaire mensuel de zéro (0) qui doit également lever une exception `ArgumentException` avec le même message.

--- 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
+++ 03.tax-simulator/c#/Tax.Simulator.Tests/SimulateurShould.cs
@@ -1,6 +1,35 @@
+using FluentAssertions;
+using Xunit;
+
 namespace Tax.Simulator.Tests;
 
 public class SimulateurShould
 {
-    // TODO j'ai pas eu le temps de faire les tests unitaires...
+    [Fact(DisplayName = "Calcul de l'impôt pour un célibataire")]
+    public void CalculerImpotsAnnuel_Celibataire()
+    {
+        string situationFamiliale = "Célibataire";
+        int salaireMensuelInvalide = -2000;
+        int salaireMensuelValide = 2000;
+        int salaireMensuelZero = 0;
+        int salaireMensuelConjoint = 0;
+        int nombreEnfants = 0;
+
+        decimal resultatValide = Simulateur.CalculerImpotsAnnuel(situationFamiliale, salaireMensuelValide, salaireMensuelConjoint, nombreEnfants);
+        
+        var exception = Assert.Throws<ArgumentException>(() => 
+        Simulateur.CalculerImpotsAnnuel(situationFamiliale, salaireMensuelInvalide, salaireMensuelConjoint, nombreEnfants));
+
+        var exceptionSalaireZero = Assert.Throws<ArgumentException>(() =>
+        Simulateur.CalculerImpotsAnnuel(situationFamiliale, salaireMensuelZero, salaireMensuelConjoint, nombreEnfants));
+
+        Assert.Equal("Les salaires doivent être positifs.", exception.Message);
+        Assert.IsType<ArgumentException>(exception);
+
+        Assert.Equal("Les salaires doivent être positifs.", exceptionSalaireZero.Message);
+        Assert.IsType<ArgumentException>(exceptionSalaireZero);
+
+        Assert.Equal((decimal)1515.25, resultatValide);
+
+    }
 }
